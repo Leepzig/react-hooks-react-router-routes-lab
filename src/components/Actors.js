af31080b7737bf99d168e0074a82540c4dec16d1
andrew <neon.duncan@gmail.com>
2021-07-14 09:14:00 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,14 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  //actors.map(actor => <div>{actor.name} <ul>{actor.movies.map( movie => <li>{movie}</li>)}</ul></div>)
+
+  return (
+    <div>
+    <h1>Actor's Page</h1>
+    { actors.map(actor => <div key={actor.name}>{actor.name} <ul>{actor.movies.map( movie => <li key={movie}>{movie}</li>)}</ul></div>)}
+    </div>)
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,14 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+ // directors.map(director => <div>{director.name} <ul>{director.movies.map(movie => <li>{movie}</li>)}</ul></div>)
+
+  return (
+    <div>
+      <h1>Director's Page</h1>
+      {directors.map(director => <div key={director.name}>{director.name} <ul>{director.movies.map(movie => <li key={movie}>{movie}</li>)}</ul></div>)}
+    </div>)
 }
 
 export default Directors;
